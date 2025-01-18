f039a2d5f38564737c9572af156a5d9d786b704c
inakys <jassomilthon@gmail.com>
2025-01-18 10:38:34 -0600

    The release date information has been added

--- cine_scraper.py
+++ cine_scraper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import re
 from tabulate import tabulate
 
+
 url = "https://cinepolis.com/proximos-estrenos"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
@@ -16,38 +17,45 @@ def split_data(text, patron):
 def get_new_releases():
     movie_id = 1
     movies_list = []
-    for movie in soup.find_all("li"):
-        data_layer = movie.find("span", class_="data-layer")
-        
-        if data_layer:
-            title = data_layer.get("data-titulo", "No available")
-            og_title_info = data_layer.get("data-titulooriginal", "No available")
-            original_title = split_data(text=og_title_info, patron=r"^(.*?)(?:\s*\([^,]+,\s*(\d{4})\))")
-            country = split_data(text=og_title_info, patron=r"\(([^,]+),")
-            year = split_data(text=og_title_info, patron=r",\s*(\d{4})\)")
-            actor = data_layer.get("data-actor", "No available")
-            rating = data_layer.get("data-clasificacion", "No available")
-            distributor = data_layer.get("data-distribuidora", "No available")
-            genre = data_layer.get("data-genero", "No available")
-            director = data_layer.get("data-director", "No available")
-            
-            movie_data = [
-                movie_id,
-                title,          # Título
-                genre,          # Género
-                rating,         # Calificación
-                original_title, # Título original
-                year,           # Año
-                director,       # Director
-                actor,          # Actor
-                country,        # País
-                distributor     # Distribuidor
-            ]
-            
-            movies_list.append(movie_data)
-            movie_id += 1
-        movies_list.sort(key=lambda x: x[1])  
 
+    for article in soup.find_all("article", class_="estrenoFecha cf"):
+        data_layer = article.find("div", class_="diaEstreno")
+        if data_layer:
+            day = data_layer.find("span").text.strip()
+            day_month = day + " " + data_layer.text.replace(day, "").strip()
+        
+        for movie in soup.find_all("li"):
+            data_layer = movie.find("span", class_="data-layer")
+            
+            if data_layer:
+                title = data_layer.get("data-titulo", "No available")
+                og_title_info = data_layer.get("data-titulooriginal", "No available")
+                original_title = split_data(text=og_title_info, patron=r"^(.*?)(?:\s*\([^,]+,\s*(\d{4})\))")
+                country = split_data(text=og_title_info, patron=r"\(([^,]+),")
+                year = split_data(text=og_title_info, patron=r",\s*(\d{4})\)")
+                actor = data_layer.get("data-actor", "No available")
+                rating = data_layer.get("data-clasificacion", "No available")
+                distributor = data_layer.get("data-distribuidora", "No available")
+                genre = data_layer.get("data-genero", "No available")
+                director = data_layer.get("data-director", "No available")
+                
+                movie_data = {
+                    'movie_id': movie_id,
+                    'title': title,
+                    'genre': genre,          
+                    'rating': rating,         
+                    'original_title': original_title, 
+                    'year': year,           
+                    'director': director,       
+                    'actor': actor,          
+                    'country': country,        
+                    'distributor': distributor,
+                    'day_month': day_month     
+                }
+                movies_list.append(movie_data)
+                movie_id += 1
+        movies_list.sort(key=lambda x: x['title']) 
+    print(movies_list)
     return movies_list
 
 def print_terminal():
@@ -63,7 +71,8 @@ def print_terminal():
                 "Director",
                 "Actor",
                 "Country",
-                "Distributor"
+                "Distributor",
+                "Date"
               ]
     
     print(tabulate(movies_list, headers=headers, tablefmt="grid"))
--- terminal_interface.py
+++ terminal_interface.py
@@ -1,9 +1,9 @@
 import click
-from cine_scraper import print_terminal
+from cine_scraper import print_terminal,get_new_releases
 
 @click.command()
 @click.option('-up', is_flag=True, help="Imprime los estrenos")
 def cinepoliscli(up):  # Cambié 'upcoming' por 'up' para que coincida con el nombre de la opción
     if up:  # Aquí también usamos 'up' para verificar el flag
-        print_terminal()
+        get_new_releases()
     
